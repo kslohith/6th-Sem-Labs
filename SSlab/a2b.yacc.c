c9eb616e31ce7b8d141d283b1f61a3b23e8b606c
K S Lohith <32676813+kslohith@users.noreply.github.com>
2019-05-04 10:23:22 +0530

    Create a2b.yacc.c

---
+++ SSlab/a2b.yacc.c
@@ -0,0 +1,27 @@
+%{
+#include<stdio.h>
+#include<stdlib.h>
+%}
+
+%token A B NL
+
+%%
+stmt: S NL {printf("valid string\n");
+             exit(0);}
+;
+S: A S B
+   | A
+;
+%%
+
+int yyerror(char *msg)
+{
+printf("invalid string\n");
+exit(0);
+}
+
+main()
+{
+printf("enter the string\n");
+yyparse();
+}
