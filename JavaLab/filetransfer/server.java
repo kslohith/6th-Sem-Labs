3a076e6b2d8776758d4b1e7240ce4fd97d047efc
K S Lohith <32676813+kslohith@users.noreply.github.com>
2019-05-14 08:28:10 -0700

    Update server.java

--- JavaLab/filetransfer/server.java
+++ JavaLab/filetransfer/server.java
@@ -3,6 +3,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
  
 public class Server extends Thread {
     public static final int PORT = 3332;
